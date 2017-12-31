b01b7761f42df72a88b0675694b01d6b592d4964
Jonas Jensen <jonas.jensen@greenbird.com>
2017-12-31 05:59:27 +0100

    test

--- api/api.js
+++ api/api.js
@@ -3,12 +3,8 @@ import { CHAT_MESSAGE } from '../shared/socketEvents';
 
 const socket = io('http://localhost:8000');
 
-const subscribe = onReceiveMessage => {
-  socket.on(CHAT_MESSAGE, onReceiveMessage);
-};
+const subscribe = onReceiveMessage => socket.on(CHAT_MESSAGE, onReceiveMessage);
 
-const dispatch = message => {
-  socket.emit(CHAT_MESSAGE, message);
-};
+const dispatch = message => socket.emit(CHAT_MESSAGE, message);
 
 export { subscribe, dispatch };
